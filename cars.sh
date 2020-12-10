52bbe4bc61187ed9b94e0cb7a66fcd7019597436
BryanPBDang <42784989+BryanPBDang@users.noreply.github.com>
2020-12-09 20:09:50 -0800

    Update cars.sh

--- cars.sh
+++ cars.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #cars.sh
 #Bryan Dang
+
+Options = 0
+
+while ["Options" -ne 3]
+
+do
+
+  echo "Type 1 to add car"
+  echo "Type 2 to List the cars in the inventory file"
+  echo "Type 3 to Quit the program"
+  echo -r menuOptions
+  
+  case "$menuOption" in
+  1) 
